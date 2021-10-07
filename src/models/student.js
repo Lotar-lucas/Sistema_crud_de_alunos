8eaf9772012d165320ec66d10db3b6da29ad0dfb
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-07 00:34:47 -0300

    Changes in the structure of existing functions (create, getAll)

--- src/models/student.js
+++ src/models/student.js
@@ -3,15 +3,15 @@ const schema = require('./schema');
 
 const create = async (RA, username, email, cpf) => {
   const connection = schema.connectionMySQL;
-  connectionFactory(connection).execute(
-    `INSERT INTO ${connection.database}.students(RA, email, password, role) VALUES (?,?,?,?)`,
-    [RA, username, email, cpf],
-  );
+  return connectionFactory(connection)
+    .execute(`INSERT INTO ${connection.database}.students(RA, username, email, CPF) VALUES (?,?,?,?)`,
+      [RA, username, email, cpf]);
 };
 
 const getAll = async () => {
   const connection = schema.connectionMySQL;
-  connectionFactory(connection).execute(`SELECT * FROM ${connection.database}.students`);
+  return connectionFactory(connection)
+    .execute(`RA, username, email, CPF FROM ${connection.database}.students`);
 };
 
 module.exports = {
