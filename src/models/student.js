cb1426513608cb125efeedcbaf03bb90594861fe
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-07 00:03:44 -0300

    Done: model layer created and function create student

--- src/models/student.js
+++ src/models/student.js
@@ -1,2 +1,14 @@
 const connectionFactory = require('./connectionFactory');
 const schema = require('./schema');
+
+const create = async (RA, username, email, cpf) => {
+  const connection = schema.connectionMySQL;
+  connectionFactory(connection).execute(
+    `INSERT INTO ${connection.database}.students(RA, email, password, role) VALUES (?,?,?,?)`,
+    [RA, username, email, cpf],
+  );
+};
+
+module.exports = {
+  create,
+};
