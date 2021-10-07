3f385ff8abb8e62084ae4339f371af09742dce80
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-06 22:18:28 -0300

    EsLint installed and server created

---
+++ src/api/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// inports
+
+const app = express();
+app.use(bodyParser.json());
+
+// rotas
+app.use();
+
+module.exports = app;
