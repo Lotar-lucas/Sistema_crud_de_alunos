d6aca0ce6cdd671f430f484c7d100497143cf91c
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-11 11:52:27 -0300

    Feat: Created the getAll function to get data from all students

--- frontend/front-end-students/src/store/index.js
+++ frontend/front-end-students/src/store/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import fetchAPI from '../services';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    studentsData: [],
+    username: '',
+    email: '',
+    cpf: '',
   },
+
   mutations: {
+    GET_ALL_STUDENTS(state, payload) {
+      state.studentsData = payload;
+    },
+    CREATE_STUDENT(state, payload) {
+      state.username = payload.username;
+      state.email = payload.email;
+      state.cpf = payload.cpf;
+    },
   },
   actions: {
+    async getStudent(context) {
+      const responseAPI = await fetchAPI('GET', 'students');
+      context.commit('GET_ALL_STUDENTS', responseAPI.studentsData);
+    },
+    async createStudent(context, datas) {
+      console.log(context);
+      const responseAPi = await fetchAPI('POST', `students/${ra}`, { username, email, cpf });
+      context.commit('CREATE_STUDENT', responseAPi);
+
+      // students/1020133/
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
