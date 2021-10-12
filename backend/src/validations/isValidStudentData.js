4e572299e192520fdadb7d47a779283d7a1173bc
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-12 16:27:49 -0300

    Done: verification done

--- backend/src/validations/isValidStudentData.js
+++ backend/src/validations/isValidStudentData.js
@@ -1,19 +1,28 @@
 const Joi = require('joi');
+const { StatusCodes } = require('http-status-codes');
 
 const isValidStudentData = (studentData) => {
   const { error } = Joi.object(
     {
-      username: Joi.string().required().max(50)
+      username: Joi.string().required().max(50).min(3)
         .not()
         .empty(),
-      email: Joi.string().required().max(50).not()
+      email: Joi.string().required().max(50)
+        .not()
         .empty(),
-      cpf: Joi.string().required().max(11).not()
+      cpf: Joi.string().required().max(11).min(11)
+        .not()
         .empty(),
     },
   ).validate(studentData);
 
-  if (error) return { isError: true, message: error.details[0].message };
+  if (error) {
+    return {
+      isError: true,
+      message: error.details[0].message,
+      code: StatusCodes.UNPROCESSABLE_ENTITY,
+    };
+  }
   return { isError: false };
 };
 
